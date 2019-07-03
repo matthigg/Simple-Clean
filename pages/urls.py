9a56711a69a66aaa87739416fde7143c8beece62
frealis <mhh129@gmail.com>
2019-07-03 09:27:54 -0400

    Add urls, views, and templates for about, contact, reviews, and services

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import about, contact, index, reviews, services
 
 urlpatterns = [
   path('', index),
+  path('about/', about),
+  path('contact/', contact),
+  path('reviews/', reviews),
+  path('services/', services),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
+def about(request):
+  return render(request, 'pages/about.html')
+
+def contact(request):
+  return render(request, 'pages/contact.html')
+
 def index(request):
   return render(request, 'pages/index.html')
+
+def reviews(request):
+  return render(request, 'pages/reviews.html')
+
+def services(request):
+  return render(request, 'pages/services.html')

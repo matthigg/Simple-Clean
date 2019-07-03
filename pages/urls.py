866fb41a12ef93c0d943e67032aa7e2883fe2ef2
frealis <mhh129@gmail.com>
2019-07-03 12:06:30 -0400

    Add dynamic links to navbar using url template tag

--- pages/urls.py
+++ pages/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import contact, index, ourwork, reviews, services
 
 urlpatterns = [
-  path('', index),
-  path('contact/', contact),
-  path('ourwork/', ourwork),
-  path('reviews/', reviews),
-  path('services/', services),
+  path('',          index,    name='index'),
+  path('contact/',  contact,  name='contact'),
+  path('ourwork/',  ourwork,  name='ourwork'),
+  path('reviews/',  reviews,  name='reviews'),
+  path('services/', services, name='services'),
 ]

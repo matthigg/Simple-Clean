d2718af8f54b762227a0f0d8851084b88399360f
frealis <mhh129@gmail.com>
2019-07-03 08:47:13 -0400

    Adjust settings.py for AWS development

--- config/settings.py
+++ config/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SIMPLECLEAN_SECRET_KEY']
-SECRET_KEY = 'aojfafalkfalfja'
+SECRET_KEY = os.environ['SIMPLECLEAN_SECRET_KEY']
+# SECRET_KEY = 'aojfafalkfalfja'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('SIMPLECLEAN_DEBUG') == '1'

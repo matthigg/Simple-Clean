2c9080fd1cb0c2904f364eeabd7e858ce0197195
frealis <mhh129@gmail.com>
2019-07-07 18:48:38 -0400

    Update navbar UI animation

--- local_static/js/base.js
+++ local_static/js/base.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const navbar = document.querySelector('.navbar');
-  const navbar_brand = document.querySelector('.navbar-brand');
   const navbar_offsetTop_initial = navbar.offsetTop;
+  const navbar_stripe = document.querySelector('.navbar-stripe')
 
   window.addEventListener('scroll', () => {
     if (window.scrollY > navbar_offsetTop_initial) {
       navbar.classList.add('navbar-minimized-true');
       navbar.classList.remove('navbar-minimized-false');
-
-      // navbar_brand.style.fontSize = '2vw';
+      navbar_stripe.classList.add('navbar-stripe-minimized-true');
+      navbar_stripe.classList.remove('navbar-stripe-minimized-false');
     } else {
       navbar.classList.add('navbar-minimized-false');
       navbar.classList.remove('navbar-minimized-true');
+      navbar_stripe.classList.add('navbar-stripe-minimized-false');
+      navbar_stripe.classList.remove('navbar-stripe-minimized-true');
 
-      // navbar_brand.style.fontSize = '3vw';
     }
   })
 })

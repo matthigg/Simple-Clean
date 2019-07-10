7bb822cdb2163ac58c9f401f8b99a54d24ff25df
frealis <mhh129@gmail.com>
2019-07-09 22:50:34 -0400

    AWS

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgres',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],

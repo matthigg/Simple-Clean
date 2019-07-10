5fbc31a64996ce13f8cb58095d5d212aae7fd03a
frealis <mhh129@gmail.com>
2019-07-10 09:56:37 -0400

    AWS

--- config/settings.py
+++ config/settings.py
@@ -76,20 +76,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
 
   # Sqlite
-  'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-  }
+  # 'default': {
+  #   'ENGINE': 'django.db.backends.sqlite3',
+  #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  # }
 
   # Postgres
-  # 'default': {
-  #   'ENGINE':   'django.db.backends.postgresql',
-  #   'NAME':     os.environ['SIMPLECLEAN_DATABASE_NAME'],
-  #   'USER':     os.environ['SIMPLECLEAN_DATABASE_USER'],
-  #   'PASSWORD': os.environ['SIMPLECLEAN_DATABASE_PASSWORD'],
-  #   'HOST':     os.environ['SIMPLECLEAN_DATABASE_HOST'],
-  #   'PORT':     os.environ['SIMPLECLEAN_DATABASE_PORT'],
-  # }
+  'default': {
+    'ENGINE':   'django.db.backends.postgresql',
+    'NAME':     os.environ['SIMPLECLEAN_DATABASE_NAME'],
+    'USER':     os.environ['SIMPLECLEAN_DATABASE_USER'],
+    'PASSWORD': os.environ['SIMPLECLEAN_DATABASE_PASSWORD'],
+    'HOST':     os.environ['SIMPLECLEAN_DATABASE_HOST'],
+    'PORT':     os.environ['SIMPLECLEAN_DATABASE_PORT'],
+  }
   
 }
 

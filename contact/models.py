f284457d6abb999a6e05fcf2dc2e5550382aa5f3
frealis <mhh129@gmail.com>
2019-07-10 13:51:49 -0400

    Change 'contact forms' to 'contact form' in admin panel

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class ContactForm(models.Model):
+  class Meta:
+    verbose_name_plural = "Contact Form"
   name = models.CharField(max_length=100)
   email = models.CharField(max_length=100)
   phone = models.CharField(max_length = 24, default=None, blank=True)

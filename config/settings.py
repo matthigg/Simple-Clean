59a94f597fb68d69036b901fb315f46a12e0e171
frealis <mhh129@gmail.com>
2019-07-10 22:23:41 -0400

    Add *.css and *.scss file, along with routes, for Contact app

--- config/settings.py
+++ config/settings.py
@@ -155,4 +155,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # locally on your own computer
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'local_static'),
+  os.path.join(BASE_DIR, 'contact/static/contact'),
 ]

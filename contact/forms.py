c878ae213b41e604c1fcd02b42151f974974bf1c
frealis <mhh129@gmail.com>
2019-07-11 11:11:38 -0400

    Added hack to get rid of label_suffix

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,14 @@
 from django.forms import ModelForm, TextInput, Textarea
 from . import models
 
+# This is a hack to get rid of the defalt label_suffix=":"
+# https://www.caktusgroup.com/blog/2018/06/18/make-all-your-django-forms-better/
+class BaseForm(ModelForm):
+  def __init__(self, *args, **kwargs):
+    kwargs.setdefault('label_suffix', '')  
+    super(BaseForm, self).__init__(*args, **kwargs)
+ModelForm = BaseForm
+
 class CreateContactForm(ModelForm):
   class Meta:
     model = models.ContactForm

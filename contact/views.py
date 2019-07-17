098fe9ee661c69845f4790a4c24ed526c702480d
matthigg <mhh129@gmail.com>
2019-07-17 11:05:41 -0400

    AWS recaptcha

--- contact/views.py
+++ contact/views.py
@@ -10,18 +10,18 @@ import requests
 def contact_submit(request):
 
   # reCAPTCHA v3
-  r = requests.post(
-    'https://www.google.com/recaptcha/api/siteverify', 
-    params={
-      'response': request.POST["g-recaptcha-response"],
-      'secret': os.environ["RECAPTCHA_SECRET_KEY"],
-    }
-  )
+  # r = requests.post(
+  #   'https://www.google.com/recaptcha/api/siteverify', 
+  #   params={
+  #     'response': request.POST["g-recaptcha-response"],
+  #     'secret': os.environ["RECAPTCHA_SECRET_KEY"],
+  #   }
+  # )
 
   # Return 'error' message if reCAPTCHA fails
-  if r.json()['success'] == False:
-      messages.add_message(request, messages.INFO, 'There was an error with your request. Please contact us at (123) 456-7890 or email us at email@email.com.')
-      return redirect('contact')
+  # if r.json()['success'] == False:
+  #     messages.add_message(request, messages.INFO, 'There was an error with your request. Please contact us at (123) 456-7890 or email us at email@email.com.')
+  #     return redirect('contact')
 
   # Use information submitted via POST request to create an instance of a contact
   # form via the CreateContactForm() class, check that it is_valid(), and if so

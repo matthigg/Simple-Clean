20e94c8d24c52da51f6c9416beea0326b3b1708e
matthigg <mhh129@gmail.com>
2019-07-17 10:59:29 -0400

    Update comments in contact/views.py

--- contact/views.py
+++ contact/views.py
@@ -9,8 +9,7 @@ import requests
 # Create your views here.
 def contact_submit(request):
 
-  # print('============= request.POST["g-recaptcha-response"]: ', request.POST['g-recaptcha-response'])
-  # r = requests.post('https://www.google.com/recaptcha/api/siteverify', params=request.POST['g-recaptcha-response'])
+  # reCAPTCHA v3
   r = requests.post(
     'https://www.google.com/recaptcha/api/siteverify', 
     params={
@@ -18,16 +17,23 @@ def contact_submit(request):
       'secret': os.environ["RECAPTCHA_SECRET_KEY"],
     }
   )
-  print('============= r: ', r.json())
+
+  # Return 'error' message if reCAPTCHA fails
+  if r.json()['success'] == False:
+      messages.add_message(request, messages.INFO, 'There was an error with your request. Please contact us at (123) 456-7890 or email us at email@email.com.')
+      return redirect('contact')
 
   # Use information submitted via POST request to create an instance of a contact
   # form via the CreateContactForm() class, check that it is_valid(), and if so
   # then save it to the database and send a notification email.
   if request.method == "POST":
     form = forms.CreateContactForm(request.POST)
+
+    # Save form submission to database
     if form.is_valid():
       form.save()
       
+      # Send email notification
       send_mail(
         'Simple Clean Contact Form Submission',
         form.cleaned_data['message'],

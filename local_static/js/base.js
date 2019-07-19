f2c8fa9d830bb19255f4ef272d9db3a3bf8c7dc6
matthigg <mhh129@gmail.com>
2019-07-19 13:59:17 -0400

    Finish JS used to populate the modal template at ourwork-modals.html with correct before & after image paths, using Django's static template tags (or rather, extracting them from the DOM using JS) to build relative paths to the correct before & after images when a user clicks on a before & after image group

--- local_static/js/base.js
+++ local_static/js/base.js
@@ -72,13 +72,20 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // Modals
+  //
   // When a <div> .section-1-baap-group element or any of its children (all of 
   // which are located in ourwork.html and together represent a 'before & after'
   // picture group) are clicked, search/scrounge for the appropriate data-id
   // attribute's value, which identifies that particular 'before & after' picture
   // group. This data-id value is then used to construct the before & after <img>
   // src attributes in the re-usable Bootstrap modal HTML markup located in 
-  // ourwork-modals.html.
+  // ourwork-modals.html. 
+  //
+  // Additionally, this JavaScript will scrounge for the relative static img path 
+  // (ie. /static/img/...) which is stored as data-src in the same HTML element 
+  // as the data-id, and the data-src attribute's value, together with the data-id 
+  // attribute's value is then used to construct a path to the before & after 
+  // images that we're looking for.
   const baap_groups = document.querySelectorAll('.section-1-baap-group');
   baap_groups.forEach((baap_group) => {
     baap_group.addEventListener('click', (event) => {
@@ -87,7 +94,10 @@ document.addEventListener('DOMContentLoaded', () => {
         while (element.parentNode) {
           element = element.parentNode;
           if (element.dataset.id) {
-            console.log('element: ', element.dataset.id);
+            const img_src_before = element.dataset.src + '/' + element.dataset.id + '-before.jpg';
+            const img_src_after = element.dataset.src + '/' + element.dataset.id + '-after.jpg';            
+            document.querySelector('.modal-baap-group-img-before').src = img_src_before;
+            document.querySelector('.modal-baap-group-img-after').src = img_src_after;
             return
           } 
         }
